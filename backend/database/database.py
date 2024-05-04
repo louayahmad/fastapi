f9e309d9246ad54d33e0561842104200cca37120
louay <louayahmad20@gmail.com>
2024-05-03 20:24:39 -0400

    mysql instead of postgres

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,25 +1,10 @@
-from typing import Annotated
-
-from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
-URL_DATABASE = "postgresql://postgres:postgres@localhost:5432/fastapiapp"
+URL_DATABASE = "mysql+pymysl://root:root@localhost:3306/banksystem"
 
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-Base.metadata.create_all(bind=engine)
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-db_dependency = Annotated[Session, Depends(get_db)]
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
+from typing import Annotated
+
+from database.database import Base, SessionLocal, engine
 from endpoints.questions import routes as questions_routes
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
 
 app = FastAPI(
     title="My App",
@@ -7,5 +11,18 @@ app = FastAPI(
     openapi_url="/openapi.json",
     redoc_url="/redoc",
 )
+
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
 
 app.include_router(questions_routes.router, tags=["Quiz"])
